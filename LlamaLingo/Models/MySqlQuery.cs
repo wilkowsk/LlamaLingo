b2537662f808737c9babd1037241f17e3243a577
woodsj1206 <47829885+woodsj1206@users.noreply.github.com>
2023-11-08 19:44:38 -0500

    Updated MySqlQuery.cs

--- LlamaLingo/Models/MySqlQuery.cs
+++ LlamaLingo/Models/MySqlQuery.cs
@@ -10,32 +10,32 @@ namespace SyncfusionBlazorAppDemo.Pages
         public static List<int> getData()
         {
             List<int> output = new List<int>();
-            try
-            {
+   //         try
+   //         {
 
-                using (SqlConnection connection = new SqlConnection("Data Source=.;Server=localhost\\TESTSERVER101023;Initial Catalog=spidertest;Trusted_Connection=Yes;"))
+   //             using (SqlConnection connection = new SqlConnection("Data Source=.;Server=localhost\\TESTSERVER101023;Initial Catalog=spidertest;Trusted_Connection=Yes;"))
 
-				{
-                    String sql = "SELECT [test]\r\n      ,[test2]\r\n      ,[test3]\r\n  FROM [dbo].[testtable]\r\n  WHERE ID=1";
+			//	{
+   //                 String sql = "SELECT [test]\r\n      ,[test2]\r\n      ,[test3]\r\n  FROM [dbo].[testtable]\r\n  WHERE ID=1";
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        connection.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                output.Add(reader.GetInt32(0));
-								output.Add(reader.GetInt32(1));
-								output.Add(reader.GetInt32(2));
-							}
-                        }
-                    }
-				}
-			} catch (Exception ex)
-            {
-                Debug.WriteLine(ex.ToString());
-            }
+   //                 using (SqlCommand command = new SqlCommand(sql, connection))
+   //                 {
+   //                     connection.Open();
+   //                     using (SqlDataReader reader = command.ExecuteReader())
+   //                     {
+   //                         while (reader.Read())
+   //                         {
+   //                             output.Add(reader.GetInt32(0));
+			//					output.Add(reader.GetInt32(1));
+			//					output.Add(reader.GetInt32(2));
+			//				}
+   //                     }
+   //                 }
+			//	}
+			//} catch (Exception ex)
+   //         {
+   //             Debug.WriteLine(ex.ToString());
+   //         }
             Random rng = new Random();
             while (output.Count < 3)
             {
